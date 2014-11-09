01319712d49362a2e97a6085d0cec374a3f4526d
Andrei Demus <andreidemus@elance-odesk.com>
2014-11-09 21:40:00 +0200

    backend improvements

--- src/remote/core/handler.clj
+++ src/remote/core/handler.clj
@@ -7,24 +7,8 @@
             [clojure.data.json :as json]
             [clojure.tools.logging :as log]))
 
-(def movies "/Users/andrei/Movies/")
-(def fifo "/tmp/fifofile2")
-
-(defn cmd [& cs]
-  (let [c (str "echo " (apply str (interpose " " cs)) " > " fifo)]
-    (log/debug c)
-    (sh "sh" "-c" c)))
-
-(defn file-to-map [file]
-  {:name (.getName file)
-   :is-directory (.isDirectory file)
-   :hash (hash file)})
-
-(defn get-movies [path]
-  (let [files (.listFiles (clojure.java.io/file (str movies path)))
-        names (map #(.getName %) files)
-        ids (map #(str (hash %)) files)]
-  (zipmap ids names)))
+(def root "/Users/andrei/Movies/")
+(def fifo "/tmp/remote_fifo")
 
 (defn resp [r]
   (json/write-str r))
@@ -32,65 +16,57 @@
 (defn ok []
   (resp {:status "ok"}))
 
-(defn pause []
-  (cmd "pause")
+(defn path-by-id[id]
+  (let [fs (file-seq (file root))]
+    (when-let [f (some #(when (= (hash %) id) %) fs)]
+      (.getPath f))))
+
+(defn cmd [& cs]
+  (let [c (str "echo " (apply str (interpose " " cs)) " > " fifo)]
+    (log/debug c)
+    (future (sh "sh" "-c" c)))
   (ok))
+
+(defn file-to-map [file]
+  {:name (.getName file)
+   :is-directory (.isDirectory file)
+   :hash (hash file)})
+
+(defn get-movies [path]
+  (let [files (.listFiles (file path))
+        names (map #(hash-map :name (.getName %) :is-directory (.isDirectory %)) files)
+        ids (map #(str (hash %)) files)]
+  (zipmap ids names)))
 
 (defn play [id]
-  (let [movie (get (get-movies "") id)
-        c (str "mplayer -quiet -slave -input file=" fifo " " movies movie)] ;; -fs
+  (let [movie (path-by-id id)
+        c (str "rm -f " fifo
+            " && mkfifo " fifo
+            " && mplayer -quiet -slave -input file=" fifo " '" movie "'")] ;; -fs
     (log/debug c)
-    (future (sh "sh" "-c" c))
+    (future (log/debug (sh "sh" "-c" c)))
     (ok)))
 
-(defn stop []
-  (cmd "quit")
-  (ok))
-
-(defn volume-up []
-  (cmd "volume +10")
-  (ok))
-
-(defn volume-down []
-  (cmd "volume -10")
-  (ok))
-
-(defn mute []
-  (cmd "mute")
-  (ok))
-
-(defn forward []
-  (cmd "seek +10 0")
-  (ok))
-
-(defn backward []
-  (cmd "seek -10 0")
-  (ok))
-
-(defn f-forward []
-  (cmd "seek +150 0")
-  (ok))
-
-(defn f-backward []
-  (cmd "seek -150 0")
-  (ok))
-
 (defn playlist []
-  (resp (get-movies "")))
+  (resp (get-movies root)))
+
+(defn dir-playlist [id]
+  (resp (get-movies (path-by-id id))))
 
 (defroutes app-routes
-  (GET "/" [] "Remote")
-  (GET "/play/:id" [id] (play id))
-  (GET "/playlist" [id] (playlist))
-  (GET "/pause" [] (pause))
-  (GET "/stop" [] (stop))
-  (GET "/volume-up" [] (volume-up))
-  (GET "/volume-down" [] (volume-down))
-  (GET "/mute" [] (mute))
-  (GET "/forward" [] (forward))
-  (GET "/backward" [] (backward))
-  (GET "/f-forward" [] (f-forward))
-  (GET "/f-backward" [] (f-backward))
+  (GET "/" [] (slurp "resources/index.html"))
+  (GET "/play/:id" [id] (play (read-string id)))
+  (GET "/playlist" [] (playlist))
+  (GET "/playlist/:id" [id] (dir-playlist (read-string id)))
+  (GET "/pause" [] (cmd "pause"))
+  (GET "/stop" [] (cmd "quit"))
+  (GET "/volume-up" [] (cmd "volume +10"))
+  (GET "/volume-down" [] (cmd "volume -10"))
+  (GET "/mute" [] (cmd "mute"))
+  (GET "/forward" [] (cmd "seek +10 0"))
+  (GET "/backward" [] (cmd "seek -10 0"))
+  (GET "/f-forward" [] (cmd "seek +150 0"))
+  (GET "/f-backward" [] (cmd "seek -150 0"))
   (route/not-found "Not Found"))
 
 (def app
