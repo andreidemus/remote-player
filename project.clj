8d630cb591bc1c25aa87202b81524aa6780032e2
Andrei Demus <andreidemus@cloud.upwork.com>
2016-12-25 18:25:57 +0200

    Update libraries

--- project.clj
+++ project.clj
@@ -2,20 +2,20 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.2.0"]
-                 [ring/ring-defaults "0.1.2"]
-                 [org.clojure/data.json "0.2.5"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [log4j "1.2.15" :exclusions [javax.mail/mail
+                 [log4j "1.2.17" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
                                               com.sun.jmx/jmxri]]
-                 [org.slf4j/slf4j-log4j12 "1.6.6"]
-                 [ring/ring-jetty-adapter "1.3.1"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [org.slf4j/slf4j-log4j12 "1.7.22"]
+                 [ring/ring-jetty-adapter "1.5.0"]]
+  :plugins [[lein-ring "0.10.0"]]
   :ring {:handler remote.core.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}}
+                        [ring-mock "0.3.0"]]}}
   :main remote.core.handler)
